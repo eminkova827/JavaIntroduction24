1a71537143f08340b55e91e7303a95e407043789
eminkova827 <157522845+eminkova827@users.noreply.github.com>
2024-01-31 21:01:46 +0200

    sum class added

---
+++ src/main/java/hw1/Sum.java
@@ -0,0 +1,10 @@
+package hw1;
+
+import java.sql.SQLOutput;
+
+public class Sum {
+    public static void main(String[] args) {
+        System.out.println(2+5);
+        System.out.println("2+5");
+    }
+}
