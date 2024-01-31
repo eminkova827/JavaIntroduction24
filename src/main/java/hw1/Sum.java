abe7e85a3c1099ba2657f31c9a8e9f9cbb9113b4
eminkova827 <157522845+eminkova827@users.noreply.github.com>
2024-01-31 21:31:15 +0200

    sum class added + class for read from console and print resut

--- src/main/java/hw1/Sum.java
+++ src/main/java/hw1/Sum.java
@@ -1,10 +1,24 @@
 package hw1;
 
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
+        //this will print the text in the console
         System.out.println(2+5);
-        System.out.println("2+5");
+        System.out.println("12+89");
+
+        //Scanner class build in method + read from console
+        System.out.println("Please enter your name:");
+        Scanner scanner= new Scanner(System.in);
+
+        //defined variable to store the user input
+        String name = scanner.nextLine();
+        //print final message
+        System.out.println("Hello " +name+ ". abc");
+
+
     }
+
 }
