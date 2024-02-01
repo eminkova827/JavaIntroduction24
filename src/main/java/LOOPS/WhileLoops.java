9022272aa86fc9944d56e3386a1a648fb314c399
eminkova827 <157522845+eminkova827@users.noreply.github.com>
2024-02-01 21:15:41 +0200

    sTRUCTURI I DANNI CHAST1

---
+++ src/main/java/LOOPS/WhileLoops.java
@@ -0,0 +1,33 @@
+package LOOPS;
+
+import java.util.Arrays;
+import java.util.Scanner;
+public class WhileLoops {
+
+    public static void main(String[] args) {
+printFactorial( n=5 );// za 0
+printFactorial (n=7); //za 1
+
+
+        System.out.println("Enter positive number: ");
+        Scanner scanner = new Scanner(System.in);
+        int userNumber = scanner.nextInt();
+
+        printFactorial(userNumber);
+    }
+
+    public static void printFactorial(int n) {
+        //variables - da sa positive
+        int factorial = 1;
+        int i = 1;
+
+        while (i<=n){
+            factorial *=i; // factorial = factorial +i;
+            i++;
+        }
+        System.out.printf("You have entered: %d . The factoriel is: %d.\n",  n, factorial);
+
+    }
+
+
+}
